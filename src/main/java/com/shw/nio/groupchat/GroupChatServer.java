b5ff9ceaf78c7529ffcbfa083fe19fb6992fcce1
HanWen.shw <shaohanwen@example.com>
2021-04-21 21:06:23 +0800

    zeroCopy and groupChat

---
+++ src/main/java/com/shw/nio/groupchat/GroupChatServer.java
@@ -0,0 +1,117 @@
+
+package com.shw.nio.groupchat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+public class GroupChatServer {
+
+    private Selector selector;
+    private ServerSocketChannel listenChannel;
+    private static final int PORT = 6689;
+
+    public GroupChatServer() {
+
+        try {
+            selector = Selector.open();
+            listenChannel = ServerSocketChannel.open();
+            listenChannel.socket().bind(new InetSocketAddress(PORT));
+            listenChannel.configureBlocking(false);
+            listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listen() {
+        System.out.println("监听线程: " + Thread.currentThread().getName());
+        try {
+            while (true) {
+                int count = selector.select();
+                if (count > 0) {
+                    //遍历得到selectionKey 集合
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey key = iterator.next();
+                        if (key.isAcceptable()) {
+                            SocketChannel sc = listenChannel.accept();
+                            sc.configureBlocking(false);
+                            //将该 sc 注册到seletor
+                            sc.register(selector, SelectionKey.OP_READ);
+                            //提示
+                            System.out.println(sc.getRemoteAddress() + " 上线 ");
+                        }
+                        if (key.isReadable()) { //通道发送read事件，即通道是可读的状态
+                            //处理读 (专门写方法..)
+                            readData(key);
+                        }
+                        iterator.remove();
+                    }
+                } else {
+                    System.out.println("等待....");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //发生异常处理....
+        }
+    }
+
+    //读取客户端消息
+    private void readData(SelectionKey key) {
+        //取到关联的channle
+        SocketChannel channel = null;
+        try {
+            channel = (SocketChannel) key.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int count = channel.read(buffer);
+            //根据count的值做处理
+            if (count > 0) {
+                //把缓存区的数据转成字符串
+                String msg = new String(buffer.array());
+                //输出该消息
+                System.out.println("form 客户端: " + msg);
+                //向其它的客户端转发消息(去掉自己), 专门写一个方法来处理
+                sendInfoToOtherClients(msg, channel);
+            }
+
+        } catch (IOException e) {
+            try {
+                System.out.println(channel.getRemoteAddress() + " 离线了..");
+                //取消注册
+                key.cancel();
+                //关闭通道
+                channel.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+                ;
+            }
+        }
+    }
+
+    //转发消息给其它客户(通道)
+    private void sendInfoToOtherClients(String msg, SocketChannel self) throws IOException {
+        System.out.println("服务器转发消息中...");
+        System.out.println("服务器转发数据给客户端线程: " + Thread.currentThread().getName());
+        //遍历 所有注册到selector 上的 SocketChannel,并排除 self
+        for (SelectionKey key : selector.keys()) {
+            Channel targetChannel = key.channel();
+            //排除自己
+            if (targetChannel instanceof SocketChannel && targetChannel != self) {
+                SocketChannel dest = (SocketChannel) targetChannel;
+                ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+                dest.write(buffer);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GroupChatServer groupChatServer = new GroupChatServer();
+        groupChatServer.listen();
+    }
+}
+
