9d6e65a4f52979b360bd6369ef6d666c0e5d155f
HanWen.shw <shaohanwen@example.com>
2021-04-22 20:37:06 +0800

    add taskQueue and Listener

--- src/main/java/com/shw/netty/simple/NettyServerHandler.java
+++ src/main/java/com/shw/netty/simple/NettyServerHandler.java
@@ -20,11 +20,56 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     //读取客户端发送的消息
     //ChannelHandlerContext ctx:上下文对象, 含有管道pipeline, 通道channel, 地址
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf buf = (ByteBuf) msg;
-        System.out.println("客户端发送的消息是：" + buf.toString(CharsetUtil.UTF_8));
-        Channel channel = ctx.channel();
-        System.out.println("客户端地址是：" + channel.remoteAddress());
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) throws Exception {
+        //比如这里我们有一个非常耗时长的业务-> 异步执行 -> 提交该channel 对应的 NIOEventLoop 的 taskQueue 中
+        //用户程序自定义的普通任务
+        ctx.channel().eventLoop().execute(new Runnable() {
+            public void run() {
+
+                try {
+                    Thread.sleep(5 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello, 客户端~(>^ω^<)喵1", CharsetUtil.UTF_8));
+                    System.out.println("channel code=" + ctx.channel().hashCode());
+                } catch (Exception ex) {
+                    System.out.println("发生异常" + ex.getMessage());
+                }
+            }
+        });
+
+        ctx.channel().eventLoop().execute(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello, 客户端~(>^ω^<)喵2", CharsetUtil.UTF_8));
+                    System.out.println("channel code=" + ctx.channel().hashCode());
+                } catch (Exception ex) {
+                    System.out.println("发生异常" + ex.getMessage());
+                }
+            }
+        });
+
+        //用户自定义定时任务 -》该任务是提交到 scheduleTaskQueue中
+        ctx.channel().eventLoop().schedule(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello, 客户端~(>^ω^<)喵3", CharsetUtil.UTF_8));
+                    System.out.println("channel code=" + ctx.channel().hashCode());
+                } catch (Exception ex) {
+                    System.out.println("发生异常" + ex.getMessage());
+                }
+            }
+        }, 5, TimeUnit.SECONDS);
+
+
+
+        System.out.println("go on ...");
+
+
+//        ByteBuf buf = (ByteBuf) msg;
+//        System.out.println("客户端发送的消息是：" + buf.toString(CharsetUtil.UTF_8));
+//        Channel channel = ctx.channel();
+//        System.out.println("客户端地址是：" + channel.remoteAddress());
     }
 
     //数据读取完毕
